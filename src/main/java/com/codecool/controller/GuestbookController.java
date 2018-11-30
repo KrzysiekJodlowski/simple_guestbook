f246bf3b36efa78a457d2ce0194ecaa6e4b02700
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-11-30 11:16:58 +0100

    Implement basic java server

--- src/main/java/com/codecool/controller/GuestbookController.java
+++ src/main/java/com/codecool/controller/GuestbookController.java
@@ -1,4 +1,18 @@
 package com.codecool.controller;
 
-public class GuestbookController {
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import java.io.IOException;
+import java.io.OutputStream;
+
+
+public class GuestbookController implements HttpHandler {
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String response = "Hello World!";
+        httpExchange.sendResponseHeaders(200, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
 }
