69c36c83c03fac37e99c463c3620dfef2aee041a
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-11-30 19:31:48 +0100

    Implement GuestbookDAOimpl

--- src/main/java/com/codecool/dao/GuestbookDAOimpl.java
+++ src/main/java/com/codecool/dao/GuestbookDAOimpl.java
@@ -3,13 +3,36 @@ package com.codecool.dao;
 import com.codecool.model.Entry;
 import com.codecool.model.Guestbook;
 
+import java.sql.*;
+import java.text.DateFormat;
+
 
 public class GuestbookDAOimpl implements GuestbookDAO {
+
+    public GuestbookDAOimpl() {}
 
     @Override
     public Guestbook loadGuestbook() {
 
-        return new Guestbook();
+        Guestbook guestbook = new Guestbook();
+        String query = "SELECT * FROM guestbook";
+
+        try {
+            Connection connection = new DBCPDataSource().getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                guestbook.addEntry(getEntry(resultSet));
+            }
+
+            statement.close();
+            connection.close();
+            resultSet.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return guestbook;
     }
 
     @Override
@@ -17,4 +40,13 @@ public class GuestbookDAOimpl implements GuestbookDAO {
 
         return false;
     }
+
+    private Entry getEntry(ResultSet resultSet) throws SQLException {
+        String content = resultSet.getString("content");
+        String name = resultSet.getString("name");
+        Date date = resultSet.getDate("date_time");
+
+        return new Entry(content, name, date);
+    }
+
 }
