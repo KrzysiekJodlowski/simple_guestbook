af1fec194246b39a8b9dd609564f158b96be1af0
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-11-30 23:55:14 +0100

    Extend GuestbookController implementation Add form to guestbook twig file Add date and time support

--- src/main/java/com/codecool/controller/GuestbookController.java
+++ src/main/java/com/codecool/controller/GuestbookController.java
@@ -6,18 +6,55 @@ import com.codecool.model.Guestbook;
 import com.codecool.view.EntriesView;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import java.io.IOException;
+
+import java.io.*;
+import java.net.URLDecoder;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class GuestbookController implements HttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
 
-        Guestbook guestbook = new GuestbookDAOimpl().loadGuestbook();
-        List<Entry> listOfEntries = guestbook.getEntries();
+        String response = "odpowiedz";
+        String method = httpExchange.getRequestMethod();
 
-        new EntriesView(httpExchange).showEntries(listOfEntries);
+        if(method.equals("GET")){
+
+            Guestbook guestbook = new GuestbookDAOimpl().loadGuestbook();
+            List<Entry> listOfEntries = guestbook.getEntries();
+
+            new EntriesView(httpExchange).showEntries(listOfEntries);
+        }
+
+        if(method.equals("POST")){
+
+            InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
+            BufferedReader br = new BufferedReader(isr);
+            String formData = br.readLine();
+
+            Map inputs = parseFormData(formData);
+
+            System.out.println(inputs.get("message") + " " + inputs.get("name"));
+
+            httpExchange.sendResponseHeaders(200, response.length());
+            OutputStream os = httpExchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    private static Map<String, String> parseFormData(String formData) throws UnsupportedEncodingException {
+        Map<String, String> map = new HashMap<>();
+        String[] pairs = formData.split("&");
+        for(String pair : pairs){
+            String[] keyValue = pair.split("=");
+            String value = new URLDecoder().decode(keyValue[1], "UTF-8");
+            map.put(keyValue[0], value);
+        }
+        return map;
     }
 
 }
--- src/main/java/com/codecool/model/Entry.java
+++ src/main/java/com/codecool/model/Entry.java
@@ -1,15 +1,15 @@
 package com.codecool.model;
 
-import java.sql.Date;
+import java.time.LocalDateTime;
 
 
 public class Entry {
 
     private String content;
     private String name;
-    private Date date;
+    private LocalDateTime date;
 
-    public Entry(String content, String name, Date date) {
+    public Entry(String content, String name, LocalDateTime date) {
         this.content = content;
         this.name = name;
         this.date = date;
@@ -23,8 +23,9 @@ public class Entry {
         return this.name;
     }
 
-    public Date getDate() {
-        return this.date;
+    public String getDate() {
+        return this.date.getDayOfMonth() + "." + this.date.getMonthValue() + "." + this.date.getYear() + " " +
+                this.date.getHour() + ":" + this.date.getMinute() + ":" + this.date.getSecond();
     }
 
 }
