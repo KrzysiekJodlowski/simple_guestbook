20b0ae955a45c0c94318b1fd40b4ae6c1c5bcb92
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-11-30 19:15:18 +0100

    Implement model - Guestbook and Entry class

--- src/main/java/com/codecool/model/Entry.java
+++ src/main/java/com/codecool/model/Entry.java
@@ -1,4 +1,18 @@
 package com.codecool.model;
 
+import java.text.DateFormat;
+
+
 public class Entry {
+
+    private String content;
+    private String name;
+    private DateFormat date;
+
+    public Entry(String content, String name, DateFormat date) {
+        this.content = content;
+        this.name = name;
+        this.date = date;
+    }
+
 }
